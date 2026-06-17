b52e4b16ccc5843df6f5b4cb40bce690ca7a139a
HendersonSilva <hendersonsilva_@hotmail.com>
2026-06-17 17:46:36 -0300

    feat: adiciona campo e logica senha

--- script.js
+++ script.js
@@ -9,7 +9,16 @@ document.getElementById('formCadastro').addEventListener('submit', function(even
 
 
     // DESENVOLVEDOR B: CAPTURE O VALOR DA SENHA E FAÇA A VALIDAÇÃO DE TAMANHO AQUI
+    const senhaInput = document.getElementById('senha');
+    const senha = senhaInput.value;
 
+    senhaInput.classList.remove('erro');
+    if (senha.length < 6) {
+        senhaInput.classList.add('erro');
+        mensagemDiv.innerText = 'A senha deve ter pelo menos 6 caracteres.';
+        mensagemDiv.style.color = 'red';
+        return;
+    }
 
     // LÓGICA DE EXIBIÇÃO
     // Ambos os alunos precisarão ajustar esta mensagem conforme suas respectivas tarefas
