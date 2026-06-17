d25ad1f0107040d98e68181d83545bb66228dae2
Geliton <gelitonfilhodev@gmail.com>
2026-06-17 17:38:41 -0300

    feat: adiciona campo e lógica de senha

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 document.getElementById('formCadastro').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
     const nome = document.getElementById('nome').value;
     const mensagemDiv = document.getElementById('mensagem');
 
     // DESENVOLVEDOR A: CAPTURE O VALOR DO INPUT DE EMAIL AQUI
 
-
     // DESENVOLVEDOR B: CAPTURE O VALOR DA SENHA E FAÇA A VALIDAÇÃO DE TAMANHO AQUI
+    const senhaInput = document.getElementById('senha');
+    const senha = senhaInput.value;
 
+    if (senha.length < 6) {
+        senhaInput.classList.add('erro');
+        mensagemDiv.innerText = 'A senha deve ter pelo menos 6 caracteres.';
+        mensagemDiv.style.color = 'red';
+        return;
+    }
+
+    senhaInput.classList.remove('erro');
 
     // LÓGICA DE EXIBIÇÃO
-    // Ambos os alunos precisarão ajustar esta mensagem conforme suas respectivas tarefas
     mensagemDiv.innerText = `Usuário ${nome} cadastrado com sucesso!`;
-    mensagemDiv.style.color = "green";
+    mensagemDiv.style.color = 'green';
 });
